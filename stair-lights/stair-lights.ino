eb7ccd82040d4cd98e03cc008f6924440bfa986f
Jason Thrasher <jasonthrasher@gmail.com>
2015-03-20 00:51:32 -0700

    implemented fader and interrupt switch

--- stair-lights/stair-lights.ino
+++ stair-lights/stair-lights.ino
@@ -7,25 +7,43 @@
 
 #define LED_GATE_PIN 5
 
-#define FADE_SPEED 10     // make this higher to slow down
+#define FADE_SPEED 5     // make this higher to slow down
+
+//#define BUTTON_PIN 2  // interrupt pin
+volatile int state = HIGH;
+
+#define A_POT_PIN 3  // analog pot pin
+int pot_val = 0;
+
 
 void setup() {
   pinMode(LED_GATE_PIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Hello world");
+  delay(2000);// Give reader a chance to see the output.
+
+  attachInterrupt(0, toggle, FALLING);
 }
 
 
 void loop() {
-  int r;
 
-  // fade on
-  for (r = 0; r < 256; r++) {
-    analogWrite(LED_GATE_PIN, r);
-    delay(FADE_SPEED);
-  }
-  // fade off
-  for (r = 255; r > 0; r--) {
-    analogWrite(LED_GATE_PIN, r);
-    delay(FADE_SPEED);
+  pot_val = analogRead(A_POT_PIN) / 4;    // read the value from the sensor
+  Serial.println(pot_val);
+  delay(100);// Give reader a chance to see the output.
+
+  if (state == HIGH) {
+    Serial.println("high");
+    Serial.println(state);
+    analogWrite(LED_GATE_PIN, pot_val);
+  } else {
+    Serial.println("low");
+    analogWrite(LED_GATE_PIN, 0);
   }
 
+}
+
+void toggle() {
+  state = !state;
 }
