02ab98391bc9b7596a928cdaa9d8f7f4256b0a5e
Jason Thrasher <jasonthrasher@gmail.com>
2015-03-22 16:28:41 -0700

    refactored to not polling the potentiometer on every loop

--- stair-lights/stair-lights.ino
+++ stair-lights/stair-lights.ino
@@ -1,3 +1,14 @@
+// This program manages the 12V LED lights installed on the stairs.
+
+// Use-cases
+// 1) manually turn the device on or off by pressing a button
+// 2) manually adjust the brightness by turning a pot
+// 3) call a URL to turn the lights on or off (manual over-rides)
+// 4) call a URL to adjust the brightness (manual over-rides)
+// 6) monitor ambient light in the stairwell and automatically turn on when it's dark
+// 7) monitor the ambient light in the stairwell and automatically turn off when it's daytime
+// 8) 
+
 // Connect LED strip as:
 // - LED to MOSFET drain
 // + LED to +12 volt supply
@@ -5,45 +16,51 @@
 // MOSFET gate to LED_GATE_PIN
 
 
-#define LED_GATE_PIN 5
-
-#define FADE_SPEED 5     // make this higher to slow down
-
-//#define BUTTON_PIN 2  // interrupt pin
-volatile int state = HIGH;
-
+#define LED_GATE_PIN 5  // pwm digital pin for MOSFET gate
 #define A_POT_PIN 3  // analog pot pin
-int pot_val = 0;
+
+volatile int state = HIGH;
+int brightness = 255;
 
 
 void setup() {
   pinMode(LED_GATE_PIN, OUTPUT);
-
+  
   Serial.begin(115200);
   Serial.println("Hello world");
   delay(2000);// Give reader a chance to see the output.
 
-  attachInterrupt(0, toggle, FALLING);
+  attachInterrupt(INT0, toggle, FALLING);
+
+  setBrightness();
 }
 
-
 void loop() {
-
-  pot_val = analogRead(A_POT_PIN) / 4;    // read the value from the sensor
-  Serial.println(pot_val);
-  delay(100);// Give reader a chance to see the output.
-
-  if (state == HIGH) {
-    Serial.println("high");
-    Serial.println(state);
-    analogWrite(LED_GATE_PIN, pot_val);
-  } else {
-    Serial.println("low");
-    analogWrite(LED_GATE_PIN, 0);
+  if (brightness != analogRead(A_POT_PIN) / 4) {
+    brightness = analogRead(A_POT_PIN) / 4;    // read the value from the sensor
+    setBrightness();  
   }
+  Serial.println(brightness);
+  delay(20);// Give reader a chance to see the output.
+}
 
+void setBrightness() {
+  Serial.print("set brightness: ");
+  Serial.print(state);
+  Serial.print(" ");
+  Serial.print(brightness);
+  
+  if (state == HIGH) {
+    // note: light can be on, but with 0 brightness
+    analogWrite(LED_GATE_PIN, brightness);
+  } else {
+    // light is off
+    digitalWrite(LED_GATE_PIN, LOW); 
+  }  
 }
 
 void toggle() {
   state = !state;
+  setBrightness();
 }
+
