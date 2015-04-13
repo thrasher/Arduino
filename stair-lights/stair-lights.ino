113d43cb9bf55f775ea804e763ebecf5471d1c14
Jason Thrasher <jasonthrasher@gmail.com>
2015-04-13 08:09:30 -0700

    Added mode support for auto/on/off with simple hysteresis

--- stair-lights/stair-lights.ino
+++ stair-lights/stair-lights.ino
@@ -1,39 +1,103 @@
 #define LED_GATE_PIN 9
 #define A_POT_PIN A0
+#define BUTTON_PIN 12
+#define THRESHOLD 30 // sensor readings above threshold will turn light off
 
 int brightness = 0;
+int button_state = 1;
+int mode = 0;  // auto=0, on=1, off=2
 
 void setup(void)
-{  
+{
   // Start Serial
   Serial.begin(115200);
 
   pinMode(LED_GATE_PIN, OUTPUT);
   //  pinMode(A_POT_PIN, INPUT); // does not need to be set
+  pinMode(BUTTON_PIN, INPUT);
 
   Serial.println(F("Started..."));
 }
 
 void loop() {
-  brightness = analogRead(A_POT_PIN) / 4;    // read the value from the sensor
-
-  analogWrite(LED_GATE_PIN, brightness);
-    // light is off
-//    digitalWrite(LED_GATE_PIN, LOW); 
-
-  if (brightness <=1 ) {
-     analogWrite(LED_GATE_PIN, 1);
-  } else if (brightness < 50) {
-     analogWrite(LED_GATE_PIN, brightness);
-  } else if (50 <= brightness) {
-     digitalWrite(LED_GATE_PIN, 0);
+  switch (mode) {
+    case 0:
+      mode_auto();
+      break;
+    case 1:
+      digitalWrite(LED_GATE_PIN, 1);
+      break;
+    case 2:
+      digitalWrite(LED_GATE_PIN, 0);
+      break;
+    default:
+      mode = 0;
   }
 
-//  analogWrite(LED_GATE_PIN, brightness);
+  button_state = digitalRead(BUTTON_PIN);
+  if (button_state == 0) {
+    //    unsigned long time;
+    //    time = millis();
+    int reps = 0;
+    while (reps < 3) {
+      flash();
+      if (digitalRead(BUTTON_PIN) == 1) {
+        mode = reps;
+        break;
+      }
+      reps++;
+      delay(1500);
+    }
+  }
+
+  delay(100);
+}
+
+void mode_auto() {
+  brightness = analogRead(A_POT_PIN) / 4;    // read the value from the sensor
+
+  //  analogWrite(LED_GATE_PIN, brightness);
+  // light is off
+  //    digitalWrite(LED_GATE_PIN, LOW);
+
+  if (brightness < 1 ) {
+    // minimum dim-ness
+    brightness = 1;
+  } else if (brightness < THRESHOLD) {
+    // accept value
+  } else if (THRESHOLD <= brightness) {
+    // too bright, turn off
+    brightness = 0;
+    mode = 2; // hysteresis hack
+  }
+
+  update_light();
+
+  //  analogWrite(LED_GATE_PIN, brightness);
   Serial.print("led command: ");
   Serial.println(brightness);
- 
+}
 
-  delay(2000);
+void update_light() {
+  if (brightness == 0) {
+    digitalWrite(LED_GATE_PIN, 0); // note: analogWrite may glitch-flash on, so we use digital here
+  } else {
+    analogWrite(LED_GATE_PIN, brightness);
+  }
+}
+
+void flash() {
+  digitalWrite(LED_GATE_PIN, 1);
+  delay(100);
+  digitalWrite(LED_GATE_PIN, 0);
+  delay(200);
+  digitalWrite(LED_GATE_PIN, 1);
+  delay(100);
+  digitalWrite(LED_GATE_PIN, 0);
+  delay(200);
+  digitalWrite(LED_GATE_PIN, 1);
+  delay(100);
+  update_light();
+  delay(600);
 }
 
